1ec2450b6decf4a0be294519631bbfec660d1735
jun scott <hjjun@scottjunui-imac.local>
2016-01-04 12:35:53 +0900

    one more round

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
     @IBOutlet weak var slider: UISlider!
-    var targetValue: Int = 0
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         startNewRound()
+        updateLabel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,13 +33,26 @@ class ViewController: UIViewController {
     }
     
     func startNewRound() {
+        round += 1
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
     }
     
+    func updateLabel() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let message = "You scored \(points) points"
         
         let alert = UIAlertController(title: "Hello, WOrld", message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
@@ -40,6 +60,7 @@ class ViewController: UIViewController {
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
         startNewRound()
+        updateLabel()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
