6111c6d6ffd20258dfe68716cf17242cfde53def
Hyung jin Jun <junhyungjin@me.com>
2016-01-06 01:32:23 +0900

    Complete Bull's Eye App

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -107,6 +108,14 @@ class ViewController: UIViewController {
     @IBAction func startOver() {
                 startNewGame()
                 updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
+    
     }
 
 
