6159e360394b11c077afe1bdf78f160d4568dfd2
jun scott <hjjun@scottjunui-imac.local>
2016-01-05 18:01:43 +0900

    supporting for 4s

--- Bullseye/AboutViewController.swift
+++ Bullseye/AboutViewController.swift
@@ -9,19 +9,29 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            if let htmlData = NSData(contentsOfFile: htmlFile) {
+            let baseURL = NSURL(fileURLWithPath:
+                NSBundle.mainBundle().bundlePath)
+            webView.loadData(htmlData, MIMEType: "text/html",
+                textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+       
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -34,5 +44,4 @@ class AboutViewController: UIViewController {
     @IBAction func close() {
         dismissViewControllerAnimated(true, completion: nil)
     }
-
 }
